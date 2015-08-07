af821f277cbae6549b02816ba1de307876e57474
iashwaniarora <iashwaniarora@yahoo.com>
2015-08-07 14:21:59 -0400

    string test

--- java-practise/src/java/StringTest/StringTest.java
+++
@@ -1,7 +0,0 @@
-package java.StringTest;
-
-public class StringTest {
-public static void main(String[] args) {
-	System.out.println("Hi Git");
-}
-}
---
+++ java-practise/src/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+
+
+public class Test {
+	
+	public static void main(String[] args) {
+		Test t1 = new Test();
+		Test t2 = new Test();
+		if(!t1.equals(t2))
+			System.out.println("t1 is not equal to t2" );
+		if(t1 instanceof Object)
+			System.out.println("t1 is object of Object");
+	}
+}
