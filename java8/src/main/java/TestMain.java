a610b23d7f13fb2a0bf7c68f66b414452e57a173
Ashwani-work <iashwaniarorawork@gmail.com>
2016-05-21 15:51:42 -0500

    commiting master

--- java8/src/main/java/TestMain.java
+++ java8/src/main/java/TestMain.java
@@ -1,5 +1,51 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
 /**
- * Created by Ashwani on 5/10/2016.
+ * Created by Ashwani on 5/11/2016.
  */
 public class TestMain {
+
+
+    private static  List<Article> articles;
+
+    public static void populateArticleList()
+    {
+        articles = new LinkedList<Article>();
+        LinkedList tags = new LinkedList();
+        tags.add("java");
+        Article article = new Article("FirstTitle","FirstAuthor",tags);
+        articles.add(article);
+
+
+    }
+
+
+    public Article getFirstJavaArticle()
+    {
+        for(Article article :articles)
+        {
+            if(article.getTags().contains("java"))
+            {
+                return article;
+            }
+
+        }
+        return null;
+
+    }
+
+    public static void main(String[] args) {
+        populateArticleList();
+        TestMain testMain = new TestMain();
+        System.out.println("TestMain.main"+testMain.getFirstJavaArticle().getAuthor());
+
+    }
+
+
+    public Optional<Article> getFirstJavaArticleUsingJava8()
+    {
+        return articles.stream().filter(article -> article.getTags().contains("java")).findFirst();
+    }
 }
