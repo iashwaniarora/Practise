fae887c20e8204e4feff87e26814af1d584fdc48
ashwani arora <ashwani_arora@dell.com>
2016-07-18 20:26:31 -0500

    Java 8 streams

---
+++ java8/src/main/java/Practise.java
@@ -0,0 +1,60 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by ashwani_arora on 7/18/2016.
+ */
+public class Practise {
+
+    public static void main(String[] args) {
+        Practise practise = new Practise();
+        practise.run();
+
+
+    }
+
+    private void run()
+    {
+        Event event = new Event(1,"SNMP");
+        Event event1 = new Event(2,"Internal");
+        List<Event> list = new LinkedList();
+        list.add(event);
+        list.add(event1);
+
+        List<Event> l=(list.stream().filter(eventObj->((Event)eventObj).getName().equals("Internal")).collect(Collectors.toList()));
+        for(Event e : l)
+        {
+            System.out.println(e.getName());
+        }
+
+    }
+    class Event{
+        Event(int id,String name)
+        {
+                                this.id= id;
+            this.name=name;
+        }
+        private  int id;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        private String name;
+
+
+    }
+}
